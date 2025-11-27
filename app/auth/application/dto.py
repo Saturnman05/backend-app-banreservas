c02d4edc23a581e7b80b41dc800d058544c318d1
Francisco Madera <franciscomaderanatera@gmail.com>
2025-11-27 12:38:32 -0400

    fix: tipado en auth/application/dto

--- app/auth/application/dto.py
+++ app/auth/application/dto.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel, EmailStr, constr
+from pydantic import BaseModel, EmailStr, StringConstraints
+from typing import Annotated
 
 
 class UserRegisterDTO(BaseModel):
-    first_name: constr(min_length=1)
-    last_name: constr(min_length=1)
+    first_name: Annotated[str, StringConstraints(min_length=1)]
+    last_name: Annotated[str, StringConstraints(min_length=1)]
     email: EmailStr
-    phone: constr(min_length=7, max_length=13)
-    username: constr(min_length=3)
-    password: constr(min_length=6)
+    phone: Annotated[str, StringConstraints(min_length=7, max_length=13)]
+    username: Annotated[str, StringConstraints(min_length=3)]
+    password: Annotated[str, StringConstraints(min_length=6)]
